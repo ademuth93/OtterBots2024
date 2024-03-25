31e1d0069f739fe6c1ef0f98fad71eab15079983
Eric <birdydog023@gmail.com>
2024-03-25 16:35:02 -0500

    Beginning to delete things but nervous

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,33 +1,21 @@
 package frc.robot;
 
-import com.pathplanner.lib.auto.AutoBuilder;
-import com.pathplanner.lib.auto.NamedCommands;
-
 import frc.robot.commands.*;
 import frc.robot.oi.LimeLight;
 import frc.robot.subsystems.*;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.XboxController;
-import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 
 public class RobotContainer {
-        
+
     // Subsystems
     private final Swerve s_Swerve = new Swerve();
-    private final LimelightVision a_LimelightVision = new LimelightVision();
-    private final NoteGrabber sNoteGrabber = new NoteGrabber();
-    private final Shooter sShooter = new Shooter();
-    private final Lift sLift = new Lift();
-    private final Climber sClimber = new Climber();
-    private final PhotonVision sPhotonVision = new PhotonVision();
 
     // Controllers
     private final XboxController driverController = new XboxController(2);
-    private final XboxController operatorController = new XboxController(3);
 
     // Driver Controls
     private final int translationAxis = XboxController.Axis.kLeftY.value;
@@ -41,23 +29,6 @@ public class RobotContainer {
     private final JoystickButton zeroGyro = new JoystickButton(driverController, XboxController.Button.kY.value);
     private final JoystickButton targetLock = new JoystickButton(driverController, XboxController.Button.kA.value);
 
-    // Operator Controls
-    private final int noteGrabberSpeed = XboxController.Axis.kLeftY.value;
-    private final int shooterSpeed = XboxController.Axis.kRightY.value;
-    private final int leftClimberSpeed = XboxController.Axis.kLeftTrigger.value;
-    private final int rightClimberSpeed = XboxController.Axis.kRightTrigger.value;
-
-    // Operator Buttons
-    private final JoystickButton leftClimberDown = new JoystickButton(operatorController,
-            XboxController.Button.kLeftBumper.value);
-    private final JoystickButton rightClimberDown = new JoystickButton(operatorController,
-            XboxController.Button.kRightBumper.value);
-    private final JoystickButton liftExtender = new JoystickButton(operatorController, XboxController.Button.kB.value);
-    private final JoystickButton liftRetractor = new JoystickButton(operatorController, XboxController.Button.kA.value);
-
-    // Autos : none
-    private final SendableChooser<Command> autoChooser;
-
     // Others
     public final Timer m_timer = new Timer();
     private final LimeLight a_limelight = new LimeLight();
@@ -65,17 +36,6 @@ public class RobotContainer {
 
     // The container for the robot. Contains subsystems, OI devices, and commands.
     public RobotContainer() {
-
-        NamedCommands.registerCommand("NoteGrabberOn", sNoteGrabber.autoGrabberOnCommand());
-        NamedCommands.registerCommand("NoteGrabberOut", sNoteGrabber.autoGrabberOutCommand());
-        NamedCommands.registerCommand("NoteGrabberOff", sNoteGrabber.autoGrabberOffCommand());
-        NamedCommands.registerCommand("Extend", sLift.autoRaiseCommand());
-        NamedCommands.registerCommand("Retract", sLift.autoLowerCommand());
-        NamedCommands.registerCommand("ShooterOut", sShooter.autoShooterOutCommand());
-        NamedCommands.registerCommand("ShooterIn", sShooter.autoShooterInCommand());
-        NamedCommands.registerCommand("ShooterOff", sShooter.autoShooterOffCommand());
-
-        NamedCommands.registerCommand("ERIC HAVING FUN", s_Swerve.followPathCommand());
 
         s_Swerve.setDefaultCommand(
                 new TeleopSwerve(
@@ -88,38 +48,9 @@ public class RobotContainer {
                         () -> a_limelight.getdegRotationToTarget(),
                         () -> driverController.getRawAxis(speedControl),
                         () -> gyroOffset));
-
         m_timer.start();
-
         configureButtonBindings();
 
-        // Auto stuff
-        autoChooser = AutoBuilder.buildAutoChooser(); //
-        SmartDashboard.putData("Auto Chooser", autoChooser);
-
-        sNoteGrabber.setDefaultCommand(
-                new TeleopNoteGrabber(
-                        sNoteGrabber,
-                        () -> operatorController.getRawAxis(noteGrabberSpeed)));
-
-        sShooter.setDefaultCommand(
-                new TeleopShooter(
-                        sShooter,
-                        () -> operatorController.getRawAxis(shooterSpeed) / 2.5));
-
-        sClimber.setDefaultCommand(
-                new TeleopClimber(
-                        sClimber,
-                        () -> operatorController.getRawAxis(leftClimberSpeed),
-                        () -> operatorController.getRawAxis(rightClimberSpeed),
-                        leftClimberDown,
-                        rightClimberDown));
-
-        sLift.setDefaultCommand(
-                new TeleopLift(
-                        sLift,
-                        liftExtender,
-                        liftRetractor));
     }
 
     private void configureButtonBindings() {
@@ -132,11 +63,7 @@ public class RobotContainer {
         gyroOffset = 0;
     }
 
-    public void setPipeline(int pipeline) {
-        a_LimelightVision.setPipeline(pipeline);
-    }
-
     public Command getAutonomousCommand() {
-        return autoChooser.getSelected();
+        return null;
     }
 }
